eb908b500a0d3ce0097c15cabccd96c553d5168d
kyusuku <147250847+kyusuku@users.noreply.github.com>
2025-02-14 02:39:38 +0700

    Add playGame() function

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-let humanScore = 0;
-let computerScore = 0;
+playGame();
 
 function getComputerChoice() {
     let rand = Math.floor(Math.random() * 3) + 1;
@@ -18,21 +17,37 @@ function getHumanChoice() {
     }
 }
 
-function playRound(humanChoice, computerChoice) {
-    if ((humanChoice === "rock" && computerChoice === "scissors") ||
-        (humanChoice === "paper" && computerChoice === "rock") ||
-        (humanChoice === "scissors" && computerChoice === "paper")) {
-        humanScore++;
-        console.log(`You win! ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)} beats ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}`);
-    } else if ((computerChoice === "rock" && humanChoice === "scissors") ||
-        (computerChoice === "paper" && humanChoice === "rock") ||
-        (computerChoice === "scissors" && humanChoice === "paper")) {
-        computerScore++;
-        console.log(`You lose! ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)} beats ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)}`);
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        playRound(getHumanChoice(), getComputerChoice());
+    }
+
+    if (humanScore > computerScore) {
+        console.log(`You won the game! ${humanScore}-${computerScore}`);
+    } else if (humanScore < computerScore) {
+        console.log(`You lost the game! ${humanScore}-${computerScore}`);
     } else {
-        if (humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissors") {
-            console.log("Draw!");
+        console.log(`The game is a draw! ${humanScore}-${computerScore}`);
+    }
+
+    function playRound(humanChoice, computerChoice) {
+        if ((humanChoice === "rock" && computerChoice === "scissors") ||
+            (humanChoice === "paper" && computerChoice === "rock") ||
+            (humanChoice === "scissors" && computerChoice === "paper")) {
+            humanScore++;
+            console.log(`You win! ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)} beats ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}`);
+        } else if ((computerChoice === "rock" && humanChoice === "scissors") ||
+            (computerChoice === "paper" && humanChoice === "rock") ||
+            (computerChoice === "scissors" && humanChoice === "paper")) {
+            computerScore++;
+            console.log(`You lose! ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)} beats ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)}`);
+        } else {
+            if (humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissors") {
+                console.log("Draw!");
+            }
         }
     }
 }
-
