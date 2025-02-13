b166c123b9e839269ec6dac635898f0314b60ada
kyusuku <147250847+kyusuku@users.noreply.github.com>
2025-02-14 02:13:05 +0700

    Add getHumanChoice() function

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ function getComputerChoice() {
     let rand = Math.floor(Math.random() * 3) + 1;
     let computerChoice = (rand === 1) ? "rock" : 
                          (rand === 2) ? "paper" : "scissors";
-    console.log(computerChoice);
     return computerChoice;
+}
+
+function getHumanChoice() {
+    let humanChoice = prompt("Rock, Paper, or Scissors?");
+    humanChoice = humanChoice.toLowerCase();
+    if (humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissors") {
+        return humanChoice;
+    } else {
+        console.log("Invalid Choice!");
+    }
 }
